98db231bbbe30fa060cfc1a7647a709afdcb1eae
Keijo Prunt <keijo.prunt@vincit.fi>
2023-01-16 13:49:50 +0200

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This is Vite/React frontend served by Node/Express api</p>
+    </div>
+  )
+}
+
+export default App
