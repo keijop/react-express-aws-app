254fc85654243594bea9e98aa677d69fdd663811
Keijo Prunt <keijo.prunt@vincit.fi>
2023-01-19 10:29:47 +0200

    Retrieve and display parameter from AWS SSM service

--- api/server.js
+++ api/server.js
@@ -1,10 +1,37 @@
 const express = require('express')
+const AWS = require('aws-sdk');
+
 const app = express()
 const port = 3000
+
+const getParameter = async () => {
+  try {
+    const ssm = new AWS.SSM({
+      region: 'eu-central-1'
+    })
+    const parameter = await  ssm.getParameter({ 
+          Name: '/Test/username', 
+          WithDecryption: false 
+      }).promise();
+
+      return parameter?.Parameter?.Value
+    
+  } catch (error) {
+    console.log('something went wrong: ', error);
+  }
+    }
+    
+let secret
+getParameter().then(result => secret = result).catch(err => console.log('Err', err))
 
 app.get('/ip', (req, res) => {
   return res.json({ip: req.ip})
 })
+
+app.get('/secret', (req, res) => {
+  return res.json({secret})
+})
+
 app.use(express.static('../frontend/dist'))
 
 const startServer = port => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 
 function App() {
   const [ip, setIp] = useState();
+  const [secret, setSecret] = useState();
 
   useEffect(() => {
     fetch('/ip')
@@ -9,6 +10,13 @@ function App() {
       .then(data => {
         setIp(data.ip)})
       .catch(err => console.log('err', err))
+
+fetch('/secret')
+      .then(response => response.json())
+      .then(data => {
+        setSecret(data.secret)})
+      .catch(err => console.log('err', err))
+
   }, [])
   
 
@@ -17,6 +25,7 @@ function App() {
       <h1>Hello World</h1>
       <p>This is Vite/React frontend served by Node/Express api</p>
       <p>Your ip is:<span style={{color: 'green'}}>{ip}</span></p>
+      <p>Secret retrieved from SSM:<span style={{color: 'green'}}>{secret}</span></p>
       
     </div>
   )
